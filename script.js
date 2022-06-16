62cd0849688c8d6e89b9fd3736bd64d03f0b9132
kcandeloni <kcandeloni@gmail.com>
2022-06-16 17:14:09 -0300

    Rotacionar carta ao clicar

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let quantCartas = 1;
+let quantCartas = 4;
 const deck = ["bobrossparrot.gif", "explodyparrot.gif", "fiestaparrot.gif", "metalparrot.gif", "revertitparrot.gif", "tripletsparrot.gif", "unicornparrot.gif"];
 
 
@@ -8,11 +8,39 @@ function startGame () {
     }
     createGame();
 }
-startGame();
+//startGame();
+createGame();
 
 function createGame () {
     document.querySelector("ul").innerHTML = "";
     for(let i = 0 ; i < quantCartas ; i++){
-        document.querySelector("ul").innerHTML += `<li><img class="verso" src="img/${deck[i]}"><img src="img/front.png"></li>`;
+        document.querySelector("ul").innerHTML += `<li onclick="desvirarCarta(this);" class="verso"><img src="img/${deck[i]}"><img src="img/front.png"></li>`;
     }
 }
+
+function desvirarCarta (elemento) {
+    elemento.classList.toggle("virada");
+}
+
+/*
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>The Window Object</h1>
+<h2>The setInterval() Method</h2>
+
+<p id="demo"></p>
+
+<script>
+setInterval(myTimer, 1000);
+
+function myTimer() {
+  const date = new Date();
+  document.getElementById("demo").innerHTML = date.toLocaleTimeString();
+}
+</script>
+
+</body>
+</html>
+*/
