baa3a161b628e3d9750574b5ddd9aa61fdfd20c3
kcandeloni <kcandeloni@gmail.com>
2022-06-16 17:39:00 -0300

    embaralha papagaios

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
-let quantCartas = 4;
-const deck = ["bobrossparrot.gif", "explodyparrot.gif", "fiestaparrot.gif", "metalparrot.gif", "revertitparrot.gif", "tripletsparrot.gif", "unicornparrot.gif"];
-
+const parrot = ["bobrossparrot.gif", "explodyparrot.gif", "fiestaparrot.gif", "metalparrot.gif", "revertitparrot.gif", "tripletsparrot.gif", "unicornparrot.gif"];
+let deck = [];
 
 function startGame () {
+    let quantCartas = 1;
     while((quantCartas % 2 !== 0) || (quantCartas > 14 ) || (quantCartas < 4) ){
-        quantCartas = prompt("Bem vindo(a) ao Parrot Card Game!!\n\nCom qauntas cartas você quer jogar?");
+        quantCartas = prompt("Bem vindo(a) ao Parrot Card Game!!\n\nCom qauntas cartas você quer jogar(de 4 até 14)?");
     }
+    createDeack(quantCartas);
     createGame();
 }
-//startGame();
-createGame();
+
+startGame();
+
+function createDeack(quant) {
+    for(let i = 0 ; i < quant/2; i++){
+        deck.push(parrot[i]);
+        deck.push(parrot[i]);
+    }
+    deck.sort(comparador); // Após esta linha, a minhaArray estará embaralhada
+}
+
+// Esta função pode ficar separada do código acima, onde você preferir
+function comparador() { 
+	return Math.random() - 0.5; 
+}
 
 function createGame () {
     document.querySelector("ul").innerHTML = "";
-    for(let i = 0 ; i < quantCartas ; i++){
+    for(let i = 0 ; i < deck.length ; i++){
         document.querySelector("ul").innerHTML += `<li onclick="desvirarCarta(this);" class="verso"><img src="img/${deck[i]}"><img src="img/front.png"></li>`;
     }
 }
